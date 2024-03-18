44e7373c478bf323528e3fb5b6e4ebf1ef680063
Ronddaa <tronddaa@gmail.com>
2024-03-18 17:56:10 +0100

    membership

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import { Routes, Route } from "react-router-dom";
 import MainPage from './MainPage';
 import OurTeam from './OurTeam';
+import Membership from './Membership';
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/team" element={<OurTeam />} />
+        <Route path="/membership" element={<Membership />} />
       </Routes>
     </>
   )
