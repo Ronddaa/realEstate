e97501b589521d5bc236c5d139dfa5a68f5d9453
Ronddaa <tronddaa@gmail.com>
2024-03-23 20:35:11 +0100

    start blog css

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,7 @@ import MainPage from './MainPage';
 import OurTeam from './OurTeam';
 import Membership from './Membership';
 import Agents from './Agents';
+import Blog from './Blog';
 import { Error404 } from './Error404';
 
 export default function App() {
@@ -14,33 +15,9 @@ export default function App() {
         <Route path="/team" element={<OurTeam />} />
         <Route path="/membership" element={<Membership />} />
         <Route path="/agents" element={<Agents />} />
-        <Route path="/*" element={<Error404 />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="*" element={<Error404 />} />
       </Routes>
     </>
   )
 }
-
-{/* <img className='modalJoinUsIMG' src={modalJoinUsIMG} alt="Building" width={287} height={757} />
-            <article>
-                <h2 className="titleModalJoinUs">join us</h2>
-                <p className="textModalJoinUs">If you are interested in partnering with us, please fill out the form below and we will be in contact.</p>
-                <form action="" className='formModalJoinUs'>
-                    <label htmlFor="inputForUserNameModalJoinUs" className="labelInputModalJoinUs">
-                        <input type="text" className="inputUserDateModalJoinUs" id="inputForUserNameModalJoinUs" required />
-                        <span className="textInInputModalJoinUs">first name</span>
-                    </label>
-                    <label htmlFor="inputForUserSNameModalJoinUs" className="labelInputModalJoinUs">
-                        <input type="text" className="inputUserDateModalJoinUs" id="inputForUserSNameModalJoinUs" required />
-                        <span className="textInInputModalJoinUs">last name</span>
-                    </label>
-                    <label htmlFor="inputForUserEmailModalJoinUs" className="labelInputModalJoinUs">
-                        <input type="email" className="inputUserDateModalJoinUs" id="inputForUserEmailModalJoinUs" required />
-                        <span className="textInInputModalJoinUs">e-mail</span>
-                    </label>
-                    <label htmlFor="inputForUserPhoneModalJoinUs" className="labelInputModalJoinUs">
-                        <input type="phone" className="inputUserDateModalJoinUs" id="inputForUserPhoneModalJoinUs" required />
-                        <span className="textInInputModalJoinUs">phone number</span>
-                    </label>
-                </form>
-                <button className='mainContactBtn mainContactBtnModalJoinUs'></button>
-            </article> */}
---
+++ src/components/Blog.jsx
@@ -0,0 +1,67 @@
+import "./Blog.css";
+import heroBlogIMG from '../assets/blogHeroIMG.webp'
+import blogLastNewsIMG from '../assets/blogLastNewsIMG.webp'
+import blogOldNewsIMG from '../assets/blogOldNewsIMG.webp'
+import Header from "./Header";
+import Footer from "./Footer";
+
+export default function Blog() {
+    return (
+        <>
+            <Header />
+            <section className="blogHeroSection">
+                <div className="container">
+                    <h1 className="titleBlog"><span>bl</span>og</h1>
+                    <div className='wrapperMainBtn wrapperMainBtnBlog'>
+                        <button className='mainContactBtn mainContactBtnBlog' id='mainContactBtnJoinUs'>Join us</button>
+                        <button className='mainContactBtn mainContactBtnBlog'>Contact agent</button>
+                    </div>
+                </div>
+                <img className="heroBlogIMG" src={heroBlogIMG} alt="building" />
+            </section>
+            <section className="ourBlogSection">
+                <div className="container wrapperLastNews">
+                    <div className="wrapperLastNewsIMG">
+                        <img className="lastNewsIMG" src={blogLastNewsIMG} alt="News" width={790} height={500} />
+                    </div>
+                    <article className="wrapperLastNewsInfo">
+                        <p className="textDateOfPublication">NOV 2023</p>
+                        <h2 className="titleLastNews">Lorem Ipsum is simply dummy text of the printing and typesetting industry. </h2>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry`s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. </p>
+                        <button className="mainContactBtn mainContactBtnNewsReadMore">read more</button>
+                    </article>
+                </div>
+                <div className="container">
+                    <ul className="wrapperOldNews">
+                        <li className="OldNewsCard">
+                            <img src={blogOldNewsIMG} alt="News" />
+                            <article className="wrapperOldNewsInfo">
+                                <p className="textDateOfPublicationOld"></p>
+                                <h3 className="titleOldNews">Lorem Ipsum is simply dummy text of the printing and typesetting industry. </h3>
+                                <a href="#" className="linkToReadAboutOldNews">read more</a>
+                            </article>
+                        </li>
+                        <li className="OldNewsCard">
+                            <img src={blogOldNewsIMG} alt="News" />
+                            <article className="wrapperOldNewsInfo">
+                                <p className="textDateOfPublicationOld"></p>
+                                <h3 className="titleOldNews">Lorem Ipsum is simply dummy text of the printing and typesetting industry. </h3>
+                                <a href="#" className="linkToReadAboutOldNews">read more</a>
+                            </article>
+                        </li>
+                        <li className="OldNewsCard">
+                            <img src={blogOldNewsIMG} alt="News" />
+                            <article className="wrapperOldNewsInfo">
+                                <p className="textDateOfPublicationOld"></p>
+                                <h3 className="titleOldNews">Lorem Ipsum is simply dummy text of the printing and typesetting industry. </h3>
+                                <a href="#" className="linkToReadAboutOldNews">read more</a>
+                            </article>
+                        </li>
+                    </ul>
+                </div>
+            </section>
+
+            <Footer />
+        </>
+    )
+}
